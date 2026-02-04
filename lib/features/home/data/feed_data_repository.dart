a29647eb9c17303290823431435548bdbf1f7647
AsunaUky <kirito.ne.batat@gmail.com>
2026-02-04 09:23:16 +0500

    Add files via upload

---
+++ lib/features/home/data/feed_data_repository.dart
@@ -0,0 +1,13 @@
+
+class FeedDataRepository {
+  Future<List<AppImageModel>> fetchData() async {
+    await Future.delayed(Duration(seconds: 2));
+    return [
+      AppImageModel(
+        id: '1',
+        url:
+            'https://avatars.mds.yandex.net/i?id=39d1c20a406dd09760c65d90b1648e42a3b55c26-5233530-images-thumbs&n=13',
+      ),
+    ];
+  }
+}
